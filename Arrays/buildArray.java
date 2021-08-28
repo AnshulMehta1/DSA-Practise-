89556bdf47bd6b74c5a87a9d9e8e07263473fc72
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-08-28 14:06:29 +0530

    Add files via upload

---
+++ Arrays/buildArray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+public class buildArray {
+    public static void main(String[] args) {
+
+        int[] nums = {0,2,1,5,3,4};
+        int[] ans=buildarr(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static int[] buildarr(int[] arr){
+        int start=0;
+        int end=arr.length;
+        int[] ans=new int[arr.length];
+        // Condition is ans[i]=arr[arr[i]]
+        for (int i=0;i<end;i++){
+            ans[i]=arr[arr[i]];
+        }
+        return ans;
+    }
+}
