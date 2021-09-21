4245ea4c965a84ec33e400b44b1be78a224bc305
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-09-21 19:59:52 +0530

    Create PascalsTriangle.java

---
+++ Dynamic-Programming/PascalsTriangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class PascalsTriangle{
+
+  public static void main(Strings []args){
+    
+      
+  }
+  public static  List<List<Integer>>  GenratePattern(int rows){
+  }
+  
+
+
+}
