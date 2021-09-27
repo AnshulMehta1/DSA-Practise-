3fd5a16d3309f3e4539d3faa4e5eff479b2d381d
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-09-27 16:53:45 +0530

    Fixed String-Longest-Prefix

--- Strings/LongestPrefix.java
+++ Strings/LongestPrefix.java
@@ -1,1 +1,17 @@
 
+class Solution {
+    public String longestCommonPrefix(String[] str) {
+        
+        String prefix = str[0];
+        for(int index=1;index<str.length;index++){
+            // Checks the index for each element if it is equal to prefix
+            while(str[index].indexOf(prefix) != 0){
+                // If true then add to the substring
+                prefix=prefix.substring(0,prefix.length()-1);
+            }
+        }
+        return prefix;
+        
+        
+    }
+}
