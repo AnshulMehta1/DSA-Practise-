3c3b20f6d8f5e618957ff85eaedb680f7d0ed8c8
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-09-28 09:06:05 +0530

    Update PascalsTriangle.java

--- Dynamic-Programming/PascalsTriangle.java
+++ Dynamic-Programming/PascalsTriangle.java
@@ -9,6 +9,32 @@ public class PascalsTriangle{
   }
   public static  List<List<Integer>>  GenratePattern(int rows){
   }
+ 
+    public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        result.add(Arrays.asList(new Integer[] {1}));
+        
+        if(numRows >= 2){
+           result.add(Arrays.asList(new Integer[] {1,1})); 
+        }
+        
+        for(int i=2; i<numRows; i++){
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+            
+            for(int j=1; j<i; j++){
+                row.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
+            }
+            
+            row.add(1);
+            result.add(row);
+        }
+        
+        return result;
+        
+    }
+
   
 
 
