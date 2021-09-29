943d91d5eca6e298c684efbe7413014ee5c4e3ff
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-09-29 19:15:50 +0530

    Update DistanceinArray.java

--- Arrays/DistanceinArray.java
+++ Arrays/DistanceinArray.java
@@ -1,14 +1,20 @@
 class Solution {
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
-          Arrays.sort(arr1);
+        // Sliding Window Approach
+        Arrays.sort(arr1);
         Arrays.sort(arr2);
-        var count = 0;
-        var j = 0;
+        
+        int count = 0;
+        int j = 0;
         for (int i = 0; i < arr1.length; i++) {
-            var min = arr1[i] - d;
-            var max = arr1[i] + d;
-            while (j < arr2.length && arr2[j] < min) j++; //can be optimized with binary search
-            if (isOutOfRange(arr2, j, min, max)) count++;
+            int min = arr1[i] - d;
+            int max = arr1[i] + d;
+            while (j < arr2.length && arr2[j] < min) {
+                j++;    
+            }
+            if (isOutOfRange(arr2, j, min, max)) {
+                count++;   
+            }
         }
         return count;
     }
