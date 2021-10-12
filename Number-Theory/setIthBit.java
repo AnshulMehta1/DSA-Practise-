26cd96f682b35fae467bf28fc791b916e62cac79
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-10-12 20:13:32 +0530

    Add files via upload

---
+++ Number-Theory/setIthBit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class setIthBit {
+    public static void main(String[] args){
+
+    }
+    public static int Ith(int num){
+        int mask = 0;
+        return num;
+    }
+}
