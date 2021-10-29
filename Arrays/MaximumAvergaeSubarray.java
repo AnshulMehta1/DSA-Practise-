721e9338843e7171738d71201f8cda67fdf38caa
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-10-29 21:25:28 +0530

    Create MaximumAvergaeSubarray.java

---
+++ Arrays/MaximumAvergaeSubarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for(int i = 0;i<k;i++)
+        {
+            sum += nums[i];
+        }
+        int max = sum;
+        for(int i = k;i<nums.length;i++)
+        {
+            sum += nums[i] - nums[i-k];
+            if(sum >= max)
+            {
+                max = sum;
+            }
+        }
+        
+        double ans = (double)max/(double)k;
+        
+        return ans;
+        
+    }
+}
