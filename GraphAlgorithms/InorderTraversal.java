297f5caa4d96d9ecdac72b17895175eea52e7dfc
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-12-11 23:04:40 +0530

    Create InorderTraversal.java

---
+++ GraphAlgorithms/InorderTraversal.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        helper(root, list);
+        return list;
+        
+    }
+    private void helper(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            // return 
+            return;
+        }
+        // Recursive Call
+        helper(node.left, list);
+        list.add(node.val);
+        helper(node.right, list);
+    }
+}
