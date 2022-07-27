e60a285475094d6ce989fbcd122bc2f1b3f1ed67
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2022-07-27 20:55:54 +0530

    Update IntersectionList.java

--- Linked-List/IntersectionList.java
+++ Linked-List/IntersectionList.java
@@ -1,1 +1,31 @@
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // A Set to keep a track of the visited nodes
+        Set<ListNode> visited=new HashSet<>();
+        while(headA!=null){
+            visited.add(headA);
+            // All the elements of Linked ListA will be put in the visited set
+            headA=headA.next;
+        }
+        while(headB!=null){
+            if(visited.contains(headB)){
+                return headB;
+            }
+            headB=headB.next;
+        }
+        
+        return null;
+    }
+}
